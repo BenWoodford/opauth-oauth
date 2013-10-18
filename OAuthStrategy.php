287a977eff7075dae301c17399e8df6c29dcaaa5
Ben Woodford <ben@woodfordnet.co.uk>
2013-10-18 19:40:55 +0100

    Possible OAuth2 Strategy

--- OAuthStrategy.php
+++ OAuthStrategy.php
@@ -1,179 +1,134 @@
 <?php
 /**
- * OAuth
+ * OAuth2 strategy for Opauth
  * 
- * Generic Opauth strategy implementing OAuth
  * More information on Opauth: http://opauth.org
  * 
  * @copyright    Copyright © 2012 U-Zyn Chua (http://uzyn.com)
  * @link         http://opauth.org
- * @package      Opauth.OAuthStrategy
+ * @package      Opauth.GitHubStrategy
  * @license      MIT License
  */
 
-class OAuthStrategy extends OpauthStrategy{
-	
-	/**
-	 * Compulsory configuration options
-	 */
-	public $expects = array(
-		'consumer_key', 		
-		'consumer_secret',
-		'request_token_url',
-		'access_token_url'
-	);
-	
-	/**
-	 * Compulsory configuration options
-	 */
-	public $defaults = array(
-		'method' => 'POST', 		// The HTTP method being used. e.g. POST, GET, HEAD etc 
-		'oauth_callback' => '{complete_url_to_strategy}oauth_callback',
-		
-		// From tmhOAuth
-		// Refer to Vendor/tmhOAuth/tmhOAuth.php for details on these
-		'user_token'					=> '',
-		'user_secret'					=> '',
-		'use_ssl'						=> true,
-		'debug'							=> false,
-		'force_nonce'					=> false,
-		'nonce'							=> false, // used for checking signatures. leave as false for auto
-		'force_timestamp'				=> false,
-		'timestamp'						=> false, // used for checking signatures. leave as false for auto
-		'oauth_version'					=> '1.0',
-		'curl_connecttimeout'			=> 30,
-		'curl_timeout'					=> 10,
-		'curl_ssl_verifypeer'			=> false,
-		'curl_followlocation'			=> false, // whether to follow redirects or not
-		'curl_proxy'					=> false, // really you don't want to use this if you are using streaming
-		'curl_proxyuserpwd'				=> false, // format username:password for proxy, if required
-		'is_streaming'					=> false,
-		'streaming_eol'					=> "\r\n",
-		'streaming_metrics_interval'	=> 60,
-		'as_header'				  		=> true,
-	);
-	
-	/**
-	 * tmhOAuth instance
-	 */
-	private $tmhOAuth;
-	
-	public function __construct($strategy, $env){
-		parent::__construct($strategy, $env);
-		
-		$this->strategy['consumer_key'] = $this->strategy['consumer_key'];
-		$this->strategy['consumer_secret'] = $this->strategy['consumer_secret'];
-		
-		require dirname(__FILE__).'/Vendor/tmhOAuth/tmhOAuth.php';
-		$this->tmhOAuth = new tmhOAuth($this->strategy);
-	}
-	
-	/**
-	 * Auth request
-	 */
-	public function request(){
-		$params = array(
-			'oauth_callback' => $this->strategy['oauth_callback'],
-			'scope' => 'read',
-			'response_type' => 'code',
-			'client_id' => $this->strategy['consumer_key'],
-		);
+/**
+ * GitHub strategy for Opauth
+ * 
+ * @package                        Opauth.GitHub
+ */
+class GitHubStrategy extends OpauthStrategy {
+        
+        /**
+         * Compulsory config keys, listed as unassociative arrays
+         */
+        public $expects = array('client_id', 'client_secret');
+        
+        /**
+         * Optional config keys, without predefining any default values.
+         */
+        public $optionals = array('redirect_uri', 'scope', 'state');
+        
+        /**
+         * Optional config keys with respective default values, listed as associative arrays
+         * eg. array('scope' => 'email');
+         */
+        public $defaults = array(
+                'redirect_uri' => '{complete_url_to_strategy}oauth2callback'
+        );
+        
+        /**
+         * Auth request
+         */
+        public function request() {
+                $url = $this->strategy['request_token_url'];
+                $params = array(
+                        'client_id' => $this->strategy['client_id'],
+                        'redirect_uri' => $this->strategy['redirect_uri']
+                );
 
-		$results =  $this->_request('GET', $this->strategy['request_token_url'], $params);
-		
-		if ($results !== false && !empty($results['oauth_token']) && !empty($results['oauth_token_secret'])){
-			if (!session_id()){
-				session_start();
-			}
+                foreach ($this->optionals as $key) {
+                        if (!empty($this->strategy[$key])) $params[$key] = $this->strategy[$key];
+                }
+                
+                $this->clientGet($url, $params);
+        }
+        
+        /**
+         * Internal callback, after OAuth
+         */
+        public function oauth2callback() {
+                if (array_key_exists('code', $_GET) && !empty($_GET['code'])) {
+                        $code = $_GET['code'];
+                        $url = $this->strategy['access_token_url']
+                        
+                        $params = array(
+                                'code' => $code,
+                                'client_id' => $this->strategy['client_id'],
+                                'client_secret' => $this->strategy['client_secret'],
+                                'redirect_uri' => $this->strategy['redirect_uri'],
+                        );
+                        if (!empty($this->strategy['state'])) $params['state'] = $this->strategy['state'];
+                        
+                        $response = $this->serverPost($url, $params, null, $headers);
+                        parse_str($response, $results);
+                        
+                        if (!empty($results) && !empty($results['access_token'])) {
+                                $user = $this->user($results['access_token']);
+                                
+                                $this->auth = array(
+                                        'uid' => $user['id'],
+                                        'info' => array(),
+                                        'credentials' => array(
+                                                'token' => $results['access_token']
+                                        ),
+                                        'raw' => $user
+                                );
+                                
+                                $this->mapProfile($user, 'username', 'user.username');
+                                $this->mapProfile($user, 'avatar_url', 'user.links.avatar');
+                                $this->mapProfile($user, 'user_id', 'user.user_id');
+                                
+                                $this->callback();
+                        }
+                        else {
+                                $error = array(
+                                        'code' => 'access_token_error',
+                                        'message' => 'Failed when attempting to obtain access token',
+                                        'raw' => array(
+                                                'response' => $response,
+                                                'headers' => $headers
+                                        )
+                                );
 
-			$_SESSION['_opauth_oauth'] = $results;
-			$this->_authorize($results['oauth_token']);
-		}
-		else{
-			$error = array(
-				'provider' => 'OAuth',
-				'code' => 'request_token_error',
-				'raw' => $results
-			);
+                                $this->errorCallback($error);
+                        }
+                }
+                else {
+                        $error = array(
+                                'code' => 'oauth2callback_error',
+                                'raw' => $_GET
+                        );
+                        
+                        $this->errorCallback($error);
+                }
+        }
+        
+        private function user($access_token) {
+                $user = $this->serverGet($this->strategy['api_user_endpoint'], array('access_token' => $access_token), null, $headers);
 
-			$this->errorCallback($error);
-		}
-	}
+                if (!empty($user)) {
+                        return $this->recursiveGetObjectVars(json_decode($user));
+                }
+                else {
+                        $error = array(
+                                'code' => 'userinfo_error',
+                                'message' => 'Failed when attempting to query API for user information',
+                                'raw' => array(
+                                        'response' => $user,
+                                        'headers' => $headers
+                                )
+                        );
 
-	/**
-	 * Receives oauth_verifier, requests for access_token and redirect to callback
-	 */
-	public function oauth_callback(){
-		if (!session_id()){
-			session_start();
-		}
-
-		$session = $_SESSION['_opauth_oauth'];
-		unset($_SESSION['_opauth_oauth']);
-		
-		$this->tmhOAuth->config['user_token'] = $session['oauth_token'];
-		$this->tmhOAuth->config['user_secret'] = $session['oauth_token_secret'];
-
-		$results =  $this->_request('POST', $this->strategy['access_token_url']);
-		
-		$this->auth = array(
-			'provider' => 'oauth',
-			'uid' => null,
-			'credentials' => array(
-				'token' => $results['oauth_token'],
-				'secret' => $results['oauth_token_secret']
-			),
-		);
-		
-		$this->callback();
-	}
-	
-	/**
-	 * Sends user to provider's site for authentication
-	 * calls back to oauth_callback() when done
-	 */
-	private function _authorize($oauth_token){
-		// Sends to provider's site
-		
-		// Simulate calls to callback
-		// For actual scenario, this should be done at the provider's site and not here
-		$this->redirect($this->strategy['oauth_callback']);
-	}
-	
-	
-	/**
-	 * Wrapper of tmhOAuth's request() with Opauth's error handling.
-	 * 
-	 * request():
-	 * Make an HTTP request using this library. This method doesn't return anything.
-	 * Instead the response should be inspected directly.
-	 *
-	 * @param string $method the HTTP method being used. e.g. POST, GET, HEAD etc
-	 * @param string $url the request URL without query string parameters
-	 * @param array $params the request parameters as an array of key=value pairs
-	 * @param string $useauth whether to use authentication when making the request. Default true.
-	 * @param string $multipart whether this request contains multipart data. Default false
-	 */	
-	private function _request($method, $url, $params = array(), $useauth = true, $multipart = false){
-		$code = $this->tmhOAuth->request($method, $url, $params, $useauth, $multipart);
-
-		if ($code == 200){
-			$response = $this->tmhOAuth->extract_params($this->tmhOAuth->response['response']);
-			return $response;		
-		}
-		else {
-			$error = array(
-				'provider' => 'OAuth',
-				'code' => $code,
-				'raw' => $this->tmhOAuth->response['response']
-			);
-
-			$this->errorCallback($error);
-			
-			return false;
-		}
-		
-		
-	}
-	
+                        $this->errorCallback($error);
+                }
+        }
 }
